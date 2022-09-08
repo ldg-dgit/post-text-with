458ac586b18f4a188a9b836a6378299c772bfb8f
LDG - DGit <hi-ldg@dgit.co>
2022-09-08 13:20:25 +0900

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPUdkNAzaDis-h1r93bS4SBBafBPEXV_Q",
+  authDomain: "post-with-3a5bd.firebaseapp.com",
+  projectId: "post-with-3a5bd",
+  storageBucket: "post-with-3a5bd.appspot.com",
+  messagingSenderId: "840878326597",
+  appId: "1:840878326597:web:7482478a51724ce56cb42c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
