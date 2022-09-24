610e8fd7d2810b84ec2caa069192ea839dddef22
LDG - DGit <hi-ldg@dgit.co>
2022-09-24 09:06:29 +0900

    add Delete and Update Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "firebase_im.js";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
-const Post = ({ postObj, isOwner }) => (
-  <div>
-    <h4>{postObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete</button>
-        <button>Edit</button>
-      </>
-    )}
-  </div>
-);
+const Post = ({ postObj, isOwner }) => {
+  const [editing, setEditing] = useState();
+  const [newPost, setNewPost] = useState(postObj.text);
+  const postTextRef = doc(dbService, "post-with", `${postObj.id}`);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure? You want to delete this post?");
+
+    if (ok) {
+      await deleteDoc(postTextRef);
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(postTextRef, {
+      text: newPost,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewPost(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type='text'
+              placeholder='Edit your post'
+              value={newPost}
+              onChange={onChange}
+              required
+            />
+            <input type='submit' value='Update Post' />
+          </form>
+
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{postObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Post;
