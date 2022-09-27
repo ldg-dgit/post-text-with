ab8f729f2ab81192c39a1969301d668a3a2ad4ad
LDG - DGit <hi-ldg@dgit.co>
2022-09-28 00:49:53 +0900

    get user post in profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,30 @@
 import { getAuth, signOut } from "firebase/auth";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, where, orderBy } from "firebase/firestore";
+import { authService, dbService } from "../firebase_im";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
   const auth = getAuth();
   const onLogOutClick = () => {
     signOut(auth);
     navigate("/");
   };
+  const getMyPosts = async () => {
+    const q = query(
+      collection(dbService, "post-with"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+  useEffect(() => {
+    getMyPosts();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
